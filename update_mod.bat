e230fd6a577c7a04cc94713f7539a6e0b0a81087
pfinnn <finn.isengardt@googlemail.com>
2024-04-30 18:22:19 +0200

    Release on NexusMods

--- update_mod.bat
+++ update_mod.bat
@@ -1,1 +1,1 @@
-Guild3ModUploader.exe update --mod 3233451290 --content ./content/ --preview ./content/maps/preview_square.png
+Guild3ModUploader.exe update --mod 3233451290 --content .\content\ --preview .\content\maps\preview_square.png
